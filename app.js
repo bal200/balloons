2b16282be92e541c25a9da945567b77f3c05471c
Bal <bal200@gmail.com>
2017-09-17 22:44:12 +0100

    The circles now clump together, and randomly inflate.  Chaned colour scheme.

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 
 
-MyCircle = function(game, x,y) {
+MyCircle = function(game, x,y, size) {
   /* Fake Sprite, so we have a physics body */
   Phaser.Sprite.call(this, game, x, y, 'fake');
   game.physics.enable(this, Phaser.Physics.ARCADE);
   this.body.drag = new Phaser.Point(2,2);
 
   /* Now the Graphics object, added as a child to our fake sprite */
+  this.size=size;
+  this.lastSize=size;
+  this.colour = 0xff0000/*red*/ ;
   this.graphic = game.add.graphics(0,0);
-  this.redrawGraphic( 100, 0xff0000/*red*/ );
+  this.redrawGraphic( this.size, this.colour);
 
   this.addChild(this.graphic);
   
@@ -17,22 +20,51 @@ MyCircle = function(game, x,y) {
 MyCircle.prototype = Object.create(Phaser.Sprite.prototype);
 MyCircle.prototype.constructor = MyCircle;
 
+MyCircle.prototype.makeLinks = function(objects, links) {
+  var p = new Phaser.Point(this.body.x, this.body.y);
+  for (var n=0; n<objects.length; n++) {
+    var dist = p.distance( objects[n].body );
+    if ((dist < 150) && ( !doesLinkExist(links, this, objects[n]) )) {
+      links.push(new MyLink(this, objects[n]));
+    }
+
+  }
+};
+
 MyCircle.prototype.redrawGraphic = function (radius, colour){
   this.graphic.clear();
   this.graphic.beginFill(colour, 0.5 /*alpha*/);
   this.graphic.drawCircle(0, 0, radius);
   this.graphic.endFill();
-}
+};
+MyCircle.prototype.checkSizeChange = function() {
+  if (this.size != this.lastSize) {
+    this.redrawGraphic(this.size, this.colour);
+    this.lastSize = this.size;
+  }
+};
 
 MyCircle.prototype.addMomentum = function (momentum){
   var v = this.body.velocity;
   v.x += momentum.x;
   v.y += momentum.y;
-}
+};
 MyCircle.prototype.stopAcceleration = function (){
   var v = this.body.velocity;
   v.x =0;
   v.y =0;
+};
+MyCircle.prototype.blowUp = function ( links ){
+  for (var n=0; n<links.length; n++) {
+    if (links[n].obj1 == this || links[n].obj2 == this){
+      game.add.tween(links[n]).to({length:links[n].length+20}, /*duration*/3000,
+        Phaser.Easing.Quadratic.InOut , /*autostart*/true, /*delay*/0, /*repeat*/0, /*yoyo*/false);
+        //.onComplete.add(cloudLoop, this);
+    }
+  }
+  game.add.tween(this).to({size:this.size+20}, /*duration*/3000,
+    Phaser.Easing.Quadratic.InOut , /*autostart*/true, /*delay*/0, /*repeat*/0, /*yoyo*/false);
+
 }
 
 
@@ -42,7 +74,7 @@ MyLink = function(obj1, obj2) {
   this.obj2 = obj2;
   this.calcLength();
   //this.length = this.curLength + game.rnd.between(-5, +5);
-  this.length=50;
+  this.length=80;
   this.momentum = 0;
   this.pullVec1 = new Phaser.Point(0,0);
   this.pullVec2 = new Phaser.Point(0,0);
@@ -65,9 +97,9 @@ MyLink.prototype.calcLength = function() {
 };
 MyLink.prototype.calcPull = function() {
   var dif = this.length - this.curLength;
-  this.momentum = squared(dif) /2;
-  if (this.momentum>800) this.momentum=800;
-  if (this.momentum<-800) this.momentum=-800;
+  this.momentum = squared(dif) /3;
+  if (this.momentum>400) this.momentum=400;
+  if (this.momentum<-400) this.momentum=-400;
 };
 
 MyLink.prototype.calcDirectionVectors = function(p1, p2) {
@@ -79,6 +111,18 @@ MyLink.prototype.pullObjects = function() {
   this.obj1.addMomentum( this.pullVec2 );
   this.obj2.addMomentum( this.pullVec1 );
 };
+
+function doesLinkExist(list, obj1, obj2) {
+  for (var n=0; n<list.length; n++) {
+    if ((list[n].obj1 == obj1) && (list[n].obj2 == obj2)) {
+      return true;
+    } 
+    else if ((list[n].obj1 == obj2) && (list[n].obj2 == obj1)) {
+      return true;
+    }
+  }
+  return false;
+}
 
 /*
 calc link curLengths
@@ -114,23 +158,28 @@ var playState = {
     /*****************************************************************************/
   create: function() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.physics.arcade.gravity.y = 50;
+    game.physics.arcade.gravity.y = 10;
 
     game.stage.backgroundColor = '#2d2d2d';
     
     this.circles=[];
-    for (var n=0; n<10; n++) {
-      this.circles[n] = new MyCircle(game, game.rnd.between(100, 800), game.rnd.between(100, 600));
-    }
     this.links=[];
-    for (n=0; n<9; n++) {
-      this.links[n] = new MyLink( this.circles[n], this.circles[n+1] );
+    for (var n=0; n<25; n++) {
+      this.circles[n] = new MyCircle(game, game.rnd.between(100, 800), 
+                                           game.rnd.between(100, 400), 100);
     }
+
+    for (n=0; n<this.circles.length; n++) {
+      this.circles[n].makeLinks(this.circles, this.links);
+    
+    }
+console.log("links: "+this.links.length);
 
   },
 
   update: function() {
     for (n=0; n<this.circles.length; n++) {
+      this.circles[n].checkSizeChange();
       this.circles[n].stopAcceleration();
     }
     
@@ -138,10 +187,16 @@ var playState = {
       this.links[n].update();
     }
 
-    this.circles[0].x=50;
-    this.circles[0].y=50;
-    this.circles[9].x=700;
-    this.circles[9].y=50;
+//    this.circles[0].x=50;
+//    this.circles[0].y=50;
+//    this.circles[9].x=700;
+//    this.circles[9].y=50;
+    if (game.rnd.between(0, 80) == 1){
+      var which = game.rnd.between(0, this.circles.length-1);
+      console.log("blowUp "+which);
+      this.circles[ which ].blowUp(this.links);
+    }
+
 
   },
 
